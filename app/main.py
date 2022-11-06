7f1fadb7c1ffafb0a40b2726fb962e377c23b69c
liangrui <liangrui@sensoro.com>
2022-11-06 22:27:13 +0800

    fix: 修复引用错误

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, APIRouter
-from app.random_string import router as random_string_router
+from random_string import router as random_string_router
 
 app = FastAPI()
 router = APIRouter()
 router.include_router(random_string_router)
 app.include_router(router)
-
